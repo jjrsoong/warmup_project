66c189976c0e243906f9f02df4f4a15acef577ad
Joshua Jia-Rui Soong <jjrsoong@magneton.cs.uchicago.edu>
2021-01-18 13:25:20 -0600

    Square Driver

--- README.md
+++ README.md
@@ -1,3 +1,11 @@
 # warmup_project
 
-testing testing did this work?
+**Driving in a Square**
+For this exercise I decided to employ a time-based approach. While potentially less accurate than the odometry approach, I chose this method because I am still familizaring myself with Turtlebot and object-oriented programing.
+
+I created two Twist() commands, one for moving in a straight line and a constant speed and the other for turning. Since my turn command goes at 30 degrees a second, I initilly had my robot turn for 3 seconds. However, I suspect that between the noise and the acceleration required for the robot to reach the 30 degrees/second, a little extra time was needed -- this was done by trial and error.
+
+Then, I simply pieced the forward() and turn() commands together via an infinite while loop. I used time.time() to keep track of the time that elapsed and when the robot should switch between forward() and turn() (and vice versa).
+
+//Code structure:
+This program uses a SquareDriver class. The run() definition keeps the program running, while the initization, the turn(), and the forward() commands are executed in the __init__ def via the time-based approach described above.
--- scripts/drive_square.py
+++ scripts/drive_square.py
@@ -1,1 +1,54 @@
 #!/usr/bin/env python3
+
+import rospy
+import time
+
+#needed for cmd/vel
+from geometry_msgs.msg import Twist
+from geometry_msgs.msg import Vector3
+from math import radians
+
+class SquareDriver(object):
+
+    def __init__(self):
+        rospy.init_node("drive_in_square")
+        self.leader = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
+
+
+        forward = Twist()
+        forward.linear.x = 0.2
+
+        turn = Twist()
+        turn.angular.z = radians(30)
+
+        # Keep going in a square
+        while True:
+            start_time = time.time()
+            # Move forward along one side of square
+            while True:
+                time_elapsed = time.time() - start_time
+                if time_elapsed >= 5:
+                    break
+                else:
+                    self.leader.publish(forward)
+            # Turn robot 90 degrees
+            while True:
+                time_elapsed = time.time() - start_time-5
+                if time_elapsed >= 3.03:
+                    break
+                else:
+                    self.leader.publish(turn)
+        #for x in range(0, 4):
+       # for x in range(0, 20):
+        #    self.leader.publish(forward)
+        #for x in range(0, 10):
+         #   self.leader.publish(turn)
+
+    def run(self):
+        rospy.spin()
+
+
+
+if __name__ == '__main__':
+    node = SquareDriver()
+    node.run()
