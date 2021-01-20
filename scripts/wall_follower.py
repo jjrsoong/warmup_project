1e1a23f3b663970ce10c690756ce3b3e9ce9b78e
Joshua Jia-Rui Soong <jjrsoong@magneton.cs.uchicago.edu>
2021-01-20 14:57:02 -0600

    Partial Wall Follower

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -20,19 +20,36 @@ class WallFollower(object):
         rospy.Subscriber("/scan", LaserScan, self.process_scan)
         self.navigator = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
-    def process_scan(self, data):
         lin = Vector3()
         ang = Vector3()
-        instruction = Twist(linear=lin,angular=ang)
-        if data.ranges[0] >= distance:
-            instruction.linear.x = 0.2
-            #instruction.angular.z = radians(0)
+        self.instruction = Twist(linear=lin,angular=ang)
+
+        #Boolean variable, set to True is in the middle of a turn
+        self.turning = False
+
+    def process_scan(self, data):
+        if data.ranges[0] >= distance and not self.turning:
+            self.instruction.linear.x = 0.5
+            self.instruction.angular.z = radians(0)
+        #elif data.ranges[0] >= distance and data.ranges[45] >= distance:
+        #    self.instruction.linear.x = 0
+        #    self.instruction.angular.z = radians(-30)
+        #elif data.ranges[0] >= distance and data.ranges[315] >= distance:
+        #    self.instruction.linear.x = 0
+        #    self.instruction.angular.z = radians(330)
+
         # Near a wall, need to turn somehow
         else:
-            #instruction.linear.x = 0
-            instruction.angular.z = radians(30)
+            # Has turned enough, can resume forward motion
+            if data.ranges[0] >= 1:
+                self.turning = False
+            else:
+                self.turning = True
 
-        self.navigator.publish(instruction)
+            self.instruction.linear.x = 0
+            self.instruction.angular.z = radians(30)
+
+        self.navigator.publish(self.instruction)
 
     def run(self):
         rospy.spin()
