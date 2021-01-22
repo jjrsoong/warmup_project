646fa1446afef9a8c33568e900cc6996eb7a196b
Joshua Jia-Rui Soong <jjrsoong@magneton.cs.uchicago.edu>
2021-01-22 00:35:18 -0600

    Wall Follower Working

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -27,37 +27,70 @@ class WallFollower(object):
         self.first = True
 
     def process_scan(self, data):
-        error = data.ranges[269] - distance
-        prop_control = 1
+        # error = data.ranges[269] - distance
+        # prop_control = 1
 
         lin = Vector3()
         ang = Vector3()
         instruction = Twist(linear=lin,angular=ang)
 
-        front = min(data.ranges[344:359])
+        front = min(min(data.ranges[344:359]), min(data.ranges[0:14]))
         left = min(data.ranges[269:314])
         frontLeft = min(data.ranges[315:345])
 
-
         if front >= distance and frontLeft >= distance and left >= distance:# and self.first:
-            print('forward')
             instruction.linear.x = 0.3
-            instruction.angular.z = radians(0)
-        elif front < distance and frontLeft < distance and left < distance:
-            print('fast turn')
+            # Just starting, got straight
+            if self.first:
+                print('just starting')
+                instruction.angular.z = radians(0)
+            # Right direction, but deviating from wall -- make adjustment
+            else:
+                print('forward + adjust')
+                instruction.angular.z = radians(-50)
+        elif front < distance:
+            self.first = False
+            print('obs in front')
             instruction.linear.x = 0
-            instruction.angular.z = radians(30)
-        elif front < distance and frontLeft < distance and left >= distance:
-            instruction.linear.x = 0.1
-            instruction.angular.z = radians(15)
+            instruction.angular.z = radians(20)
+        # elif front < distance and frontLeft < distance and left >= distance:
+        #     instruction.linear.x = 0.1
+        #     instruction.angular.z = radians(15)
+        # No obstacle in front but too close to left hand side -- slow adjustment
         elif front >= distance and frontLeft >= distance and left < distance:
+            self.first = False
             print('slow turn 1 ')
-            instruction.linear.x = 0.1
-            instruction.angular.z = radians(15)
+            error_rate = (distance - frontLeft) * 125
+            prop_control = 1
+            instruction.angular.z = radians(error_rate * prop_control)
+
+            # If the distance is roughly on target (implied by low angular z),
+            # then can begin to move faster. Otherwise, move slowly.
+            if (error_rate * prop_control) < 5:
+                instruction.linear.x = 0.3
+            else:
+                instruction.linear.x = 0.1
+
+        # Obstacle in front left -- slow adjustment
         elif front >= distance and frontLeft < distance and left < distance:
+            self.first = False
             print('slow turn 2')
-            instruction.linear.x = 0.1
-            instruction.angular.z = radians(15)
+            instruction.linear.x = 0.2
+            instruction.angular.z = radians(10)
+        # Major obstacle -- stop forward motion, only turn
+        # elif front < distance and frontLeft < distance and left < distance:
+        #     self.first = False
+        #     print('fast turn')
+        #     instruction.linear.x = 0
+        #     instruction.angular.z = radians(30)
+
+
+            # if frontLeft < distance:
+            #     if left < distance:
+            #         print('fast turn')
+            #         instruction.linear.x = 0
+            #         instruction.angular.z = radians(30)
+
         # elif left
 
         # elif data.ranges[0] >= distance and not self.turning:
