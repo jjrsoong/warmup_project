50a077530e3144cbcbece7fd8b672d7a7bd3320f
Joshua Jia-Rui Soong <jjrsoong@magneton.cs.uchicago.edu>
2021-01-22 00:57:53 -0600

    Wall Follower Working TODO testing

--- scripts/wall_follower.py
+++ scripts/wall_follower.py
@@ -20,43 +20,39 @@ class WallFollower(object):
         rospy.Subscriber("/scan", LaserScan, self.process_scan)
         self.navigator = rospy.Publisher("/cmd_vel", Twist, queue_size=10)
 
-        #Boolean variable, set to True is in the middle of a turn
-        self.turning = False
-
-        #Boolean variable, set to True is bot has never turned before
+        #Boolean variable, set to True if bot has never turned before
+        # (ie. it just spawned into the world and has not encountered a wall)
         self.first = True
 
     def process_scan(self, data):
-        # error = data.ranges[269] - distance
-        # prop_control = 1
-
         lin = Vector3()
         ang = Vector3()
         instruction = Twist(linear=lin,angular=ang)
 
+        #LIDAR ranges. Bot is designed to follow a wall on its left side
         front = min(min(data.ranges[344:359]), min(data.ranges[0:14]))
         left = min(data.ranges[269:314])
         frontLeft = min(data.ranges[315:345])
+        right = data.ranges[89]
 
-        if front >= distance and frontLeft >= distance and left >= distance:# and self.first:
+        if front >= distance and frontLeft >= distance and left >= distance:
             instruction.linear.x = 0.3
-            # Just starting, got straight
+            # Just entered the world, go straight ahead
             if self.first:
                 print('just starting')
                 instruction.angular.z = radians(0)
-            # Right direction, but deviating from wall -- make adjustment
+            # Right direction, but deviating from wall -- make an adjustment
             else:
                 print('forward + adjust')
                 instruction.angular.z = radians(-50)
+        # Obstacle in front, need to stop moving forward and turn
         elif front < distance:
             self.first = False
             print('obs in front')
             instruction.linear.x = 0
             instruction.angular.z = radians(20)
-        # elif front < distance and frontLeft < distance and left >= distance:
-        #     instruction.linear.x = 0.1
-        #     instruction.angular.z = radians(15)
         # No obstacle in front but too close to left hand side -- slow adjustment
+        # using proportional control
         elif front >= distance and frontLeft >= distance and left < distance:
             self.first = False
             print('slow turn 1 ')
@@ -77,50 +73,35 @@ class WallFollower(object):
             print('slow turn 2')
             instruction.linear.x = 0.2
             instruction.angular.z = radians(10)
-        # Major obstacle -- stop forward motion, only turn
-        # elif front < distance and frontLeft < distance and left < distance:
-        #     self.first = False
-        #     print('fast turn')
-        #     instruction.linear.x = 0
-        #     instruction.angular.z = radians(30)
 
+    # -- BELOW HAS NEVER BEEN TESTED --
+        # Theoretically should never occur. This means that there is a wall
+        # at the front left **only**. As a failsafe, start turning so the wall
+        # is directly left of the bot
+        elif front >= distance and frontLeft < distance and left >= distance:
+            self.first = False
+            print('Houston, we have a problem')
+            instruction.linear.x = 0
+            instruction.angular.z = radians(10)
 
-            # if frontLeft < distance:
-            #     if left < distance:
-            #         print('fast turn')
-            #         instruction.linear.x = 0
-            #         instruction.angular.z = radians(30)
-
-        # elif left
-
-        # elif data.ranges[0] >= distance and not self.turning:
-        #     print('out of first turn')
-        #     instruction.linear.x = 0.3
-        #     #self.instruction.angular.z = radians(0)
-        #
-        #     # moving forward but still getting closer to the wall
-        #     #if data.ranges[269] < distance:
-        #     instruction.angular.z = error * prop_control
-        #     print(error)
-        # #elif data.ranges[0] >= distance and data.ranges[45] >= distance:
-        # #    self.instruction.linear.x = 0
-        # #    self.instruction.angular.z = radians(-30)
-        # #elif data.ranges[0] >= distance and data.ranges[315] >= distance:
-        # #    self.instruction.linear.x = 0
-        # #    self.instruction.angular.z = radians(330)
-        #
-        # # Near a wall, need to turn somehow
-        # else:
-        #     self.first = False
-        #     self.turning = True
-        #     instruction.linear.x = 0
-        #     instruction.angular.z = error * prop_control
-        #     print(error)
-        #
-        #     if data.ranges[269] < distance and data.ranges[0] > distance:
-        #         self.turning = False
-        #         print('stop turning')
-
+        # There is a wall on the right hand side
+        if right < distance:
+            print('Houston, there is an asteroid to the right')
+            # There is no wall on the left; since the bot is optimized to
+            # follow walls on its left, spin the bot around
+            if left >= 500:
+                instruction.angular.z = radians(-90)
+            # Right hand side wall is closer than left hand side wall
+            elif left >= distance:
+                instruction.angular.z = radians(-20)
+            # Walls on both sides are close by (ie in a hall)
+            else:
+                instruction.linear.x = 0.05
+                # Right wall marginally closer than left
+                if right < left:
+                    instruction.angular.z = radians(-1)
+                else:
+                    instruction.angular.z = radians(1)
 
         print(front, frontLeft, left)
         self.navigator.publish(instruction)
